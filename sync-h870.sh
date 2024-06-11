31eb67429781c9339401f7b4aa9a518bf455ac33
Pascal M <oslinux@free.fr>
2024-06-11 11:36:25 +0200

    Update sync-h870.sh

--- sync-h870.sh
+++ sync-h870.sh
@@ -8,7 +8,7 @@ rm -rf device/lge/msm8996-common/
 rm -rf device/lge/g6-common/
 rm -rf device/lge/h870/
 
-$HOME/bin/repo sync -c -n -j4 --no-clone-bundle --no-tags && $HOME/bin/repo sync -c -l -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+$HOME/bin/repo sync -c -l -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 ## as sync failed regularly...
 $HOME/bin/repo sync -c -j1 --fail-fast --force-sync --no-clone-bundle --no-tags
