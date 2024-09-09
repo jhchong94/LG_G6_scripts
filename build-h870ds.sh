3ce1c368ecbf22c75032090fe47c06e48cd3c24d
JHC <79599816+jhchong94@users.noreply.github.com>
2024-09-09 14:00:03 +0100

    Update and rename build-h870.sh to build-h870ds.sh
    DS-ify

--- build-h870ds.sh
+++ build-h870ds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source scripts/sync-h870.sh
+source scripts/sync-h870ds.sh
 
 source build/envsetup.sh
 
@@ -7,4 +7,4 @@ source scripts/fixes.sh
 
 make clean
 
-brunch h870
+brunch h870ds
