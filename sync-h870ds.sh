3603ab754d3eaa230cc79472c5f327afe538ae34
JHC <79599816+jhchong94@users.noreply.github.com>
2024-09-10 15:23:34 +0100

    Update sync-h870ds.sh

--- sync-h870ds.sh
+++ sync-h870ds.sh
@@ -6,7 +6,7 @@ rm -rf frameworks/base/
 rm -rf kernel/lge/msm8996/
 rm -rf device/lge/msm8996-common/
 rm -rf device/lge/g6-common/
-rm -rf device/lge/h870/
+rm -rf device/lge/
 
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
