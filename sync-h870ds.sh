c1e4e5d9b5ceff6cece2d950f4ddfbdf4c0342d3
JHC <79599816+jhchong94@users.noreply.github.com>
2024-09-11 19:02:55 +0100

    Update sync-h870ds.sh

--- sync-h870ds.sh
+++ sync-h870ds.sh
@@ -8,7 +8,7 @@ rm -rf device/lge/msm8996-common/
 rm -rf device/lge/g6-common/
 rm -rf device/lge/
 
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c -j3 --force-sync --no-clone-bundle --no-tags
 
 ## as sync failed regularly...
 repo sync -c -j1 --fail-fast --force-sync --no-clone-bundle --no-tags
